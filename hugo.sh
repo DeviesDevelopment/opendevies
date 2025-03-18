4ac2983d762e53e7e213889eb369b1d83f7b3685
Rickard Andersson <dunklas@gmail.com>
2025-03-18 10:26:01 +0100

    Add --rm and --name args to docker
    To prevent new containers from being created each time running the script

--- hugo.sh
+++ hugo.sh
@@ -3,6 +3,8 @@
 HUGO_VERSION="0.145.0"
 
 docker run \
+        --name opendevies \
+        --rm \
         -p 1313:1313 \
         -v ${PWD}:/src \
         hugomods/hugo:exts-"$HUGO_VERSION" "$@"
